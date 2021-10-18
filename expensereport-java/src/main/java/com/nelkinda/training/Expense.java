80770b8a0301e5e51214843ed0ae0b0cb8a565bb
Hacene KASDI <hacene.upmc@gmail.com>
2021-10-18 23:22:52 +0200

    [STEP 2] : Refactor Expense class - Extract magic numbers

--- expensereport-java/src/main/java/com/nelkinda/training/Expense.java
+++ expensereport-java/src/main/java/com/nelkinda/training/Expense.java
@@ -1,13 +1,19 @@
 package com.nelkinda.training;
 
 class Expense {
+    private static final String X_MARKER = "X";
+    private static final String NO_MARKER = " ";
+    private static final int DINNER_MAX_AMOUNT = 5000;
+    private static final int BREAKFAST_MAX_AMOUNT = 1000;
+
     ExpenseType type;
     int amount;
 
     public String computeMealOverExpensesMarker() {
-        return this.type == ExpenseType.DINNER && this.amount > 5000
-                || this.type == ExpenseType.BREAKFAST && this.amount > 1000 ?
-                "X" :
-                " ";
+        return this.type ==
+                ExpenseType.DINNER && this.amount > DINNER_MAX_AMOUNT ||
+                this.type == ExpenseType.BREAKFAST && this.amount > BREAKFAST_MAX_AMOUNT ?
+                X_MARKER :
+                NO_MARKER;
     }
 }
